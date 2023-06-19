e6a608e59eef3f6dc64d8e5d9e89103737fdecf0
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-06-19 16:42:16 +0200

    Add users controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,7 +7,7 @@ const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select('-password').lean();
 
   if (!users?.length) {
-    return res.status(400).json({ message: 'No users found.' });
+    return res.status(204).json({ message: 'No users found.' });
   }
 
   return res.status(200).json(users);
